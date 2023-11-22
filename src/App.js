f43ba133e3ac86b1f2a4945ea894403b81731792
Szymon Sarnowicz <szymon.sarnowicz@gm.com>
2023-11-22 00:44:02 -0600

    update custom CNAME

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import Typed from "react-typed"
+
 function App() {
   return (
     <div className="app">
       <section className="main">
-      <h1>Hi, 👋 I'm Szymon Sarnowicz</h1>
-      <h3>Welcome To My Portfolio</h3>
+        <h1>Hi, 👋 I'm Szymon Sarnowicz</h1>
+        <p className="intro-subtitle">
+          <Typed
+            strings={[
+              "Full Stack Developer",
+              "Program Manager",
+              "Software Engineer"
+            ]}
+            typeSpeed={80}
+            backDelay={1100}
+            backSpeed={30}
+            loop
+          />
+        </p>
       </section>
     </div>
   );

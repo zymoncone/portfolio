e6cf93a7b3217b5a64fe90f870c23f097d791d95
Szymon Sarnowicz <szymon.sarnowicz@gm.com>
2023-11-21 23:27:09 -0600

    Include title to view intial site

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="app">
+      <section className="main">
+      <h1>Hi, 👋 I'm Szymon Sarnowicz</h1>
+      <h3>Welcome To My Portfolio</h3>
+      </section>
+    </div>
+  );
+}
+
+export default App;

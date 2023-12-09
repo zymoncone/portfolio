0249dcb5e17f4fb16c8a616b7939696f0f934a55
Szymon Sarnowicz <szymon.sarnowicz@gm.com>
2023-12-08 21:02:16 -0600

    fix pdf rendering on deploy and include download and pdf view button

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,7 @@
 import "./Resume.css"
 import resume from "../../media/Resume_Sarnowicz_Szymon.pdf"
 import { Document, Page, pdfjs } from 'react-pdf'
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString()
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 
 const Resume = () => {
   return (
@@ -12,19 +9,26 @@ const Resume = () => {
       <Document
         file={resume}
         onLoadError={console.error}
+        style={{width:"90vw"}}
       >
         <Page 
           pageIndex={0}
           renderAnnotationLayer={false}
           renderTextLayer={false}
+          width={window.innerWidth*0.9}
           className="custom-doc"/>
       </Document>
       <div className="download-button" 
         ><a href={resume} 
             download="resume_szymon_sarnowicz"
+            >download</a>
+      </div>
+      <div className="download-button" 
+        ><a href={resume}
             rel="noreferrer"
             target="_blank"
-            >download resume</a></div>
+            >view in pdf reader</a>
+      </div>
     </div>
   )
 }
